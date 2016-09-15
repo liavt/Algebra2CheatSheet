f020ec22aaef3eae4e46fc85c49627bfe590a39e
Liav Turkia <liavt@yahoo.com>
2016-09-15 14:35:34 +0000

    Probability is done

--- probability.js
+++ probability.js
@@ -1,15 +1,45 @@
-var probalityRow = '<tr><td><input class="good-combos"type="number"min="2"></td><td><input class="possible-combos"type="number"min="2"></td><td><select><option value="ind">Independent</option><option value="de">Dependent</option></select></td></tr>';
+/*global $ */
 
-function reduce(numerator,denominator){
-  var gcd = function gcd(a,b){
+var probalityRow = '<tr><td><input class="good-combos"type="number"min="2"></td><td><input class="possible-combos"type="number"min="2"></td><td><select class="possibility-type"><option value="ind">Independent</option><option value="de">Dependent</option></select></td></tr>';
+
+function gcd(a,b){
     return b ? gcd(b, a%b) : a;
-  };
-  gcd = gcd(numerator,denominator);
-  return [numerator/gcd, denominator/gcd];
 }
 
-function calculateProbability(possibilities){
+function reduce(numerator,denominator){
+  var gcdResult = gcd(numerator,denominator);
+  return [numerator/gcdResult, denominator/gcdResult];
+}
+
+function lcm(denominators){
+    var result = denominators[0];
+    for(var i = 1;i<denominators.length;i++){
+        result = Math.abs(result*denominators[i]) / gcd(result,denominators[i]);
+    }
+    return result;
+}
+
+function calculateProbability(numerators,denominators,types){
+    var commonMultiple = lcm(denominators);
+    for(var i =0;i<numerators.length;i++){
+        var multiplier = denominators[i]/commonMultiple;
+        console.log(multiplier);
+        numerators[i] /=multiplier;
+        denominators[i] = commonMultiple;
+    }
     
+    var result = 0;
+        
+    for(var i =0;i<types.length;i++){
+        var type = types[i];
+        if(type=='ind'){
+            result+=Number(numerators[i]/denominators[i]);
+        }else if(type=='de'){
+            result*=Number(numerators[i]/denominators[i]);
+        }
+    }
+    
+    return result;
 }
 
 $(document).ready(function(){
@@ -27,10 +57,29 @@ $(document).ready(function(){
     
     $('#calc-submit').click(function(){
         var out='';
+        
+        var goodCombos = [];
+        var possibleCombos = [];
+        var types = [];
+        
         $('#calc-table td').each(function(){
-            out+=$('.good-combos',this).val();
+            var currentGoodCombo = $('.good-combos',this).val();
+            if(currentGoodCombo)goodCombos.push(currentGoodCombo);
+            
+            var currentPossibleCombo = $('.possible-combos',this).val();
+            if(currentPossibleCombo)possibleCombos.push(currentPossibleCombo);
+            
+            var currentType = $('.possibility-type',this).val();
+            if(currentType)types.push(currentType);
         });
-        $('#calc-result').html(out); 
+        
+        var result = calculateProbability(goodCombos,possibleCombos,types).toFixed(2);
+        
+        if(result>1)result=1;
+        
+        var fraction = reduce((result*100),100);
+        
+        $('#calc-result').html((result*100)+'% probability of success.<br>Fraction: '+fraction[0]+'/'+fraction[1]+'<br>Decimal result: '+result); 
     });
     
     $('#dice-submit').click(function(){
